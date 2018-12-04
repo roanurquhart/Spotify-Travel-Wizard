66913a12cebb85b93a281b9e48e024c5d3b4600c
Roan Urquhart <roanurquhart@gmail.com>
2018-12-04 16:11:38 -0500

    Add existing file

---
+++ travelscript.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+buildHome()
+});
+
+let log_url = 'https://accounts.spotify.com/authorize?client_id=058d8c0bc3fd40bab5e2d2cf01fc8bc1&redirect_uri=file%3A%2F%2F%2FUsers%2Froanroan%2FDocuments%2F3rd%2520Year%2520-%25201st%2520Semester%2FCOMP%2520426%2FFinalProject%2Ftravel.html&scope=user-read-private%20user-read-email&response_type=token&state=123';
+
+function login() {
+  window.location = log_url;
+}
+
+function buildHome() {
+  let spot_box = '<div class="spotify_container"><p class="spotify_text">Connect to Spotify</p><button type="button" class="spotify_ajax" onclick="login(); return false;">Connect</button></div>';
+  let header = '<header class="page_head">The Travel Wizard<header>';
+  let choose = '<p>Choose your destiny</p>';
+  let choice_box = '<div id="choice_box"><p class="choices evil"><span id="evil">Air Travel Overlord</span></p><span>   </span><p class="choices good"><span id="good">Naive Vacationer</span></p></div>'
+
+  body = $('body');
+
+  body.append(header);
+  body.append(choose);
+  body.append(choice_box);
+}
